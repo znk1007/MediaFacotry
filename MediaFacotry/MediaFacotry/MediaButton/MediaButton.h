b1dff77d5b25a56acabb17bdce04888722f105c0
znk1007 <1032479745@qq.com>
2017-11-27 17:53:04 +0800

    0.0.1

--- MediaFacotry/MediaFacotry/MediaButton/MediaButton.h
+++ MediaFacotry/MediaFacotry/MediaButton/MediaButton.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+    /**静默加载*/
+    MediaFactoryImageOptionsNormal,
+    /**遮罩加载*/
+    MediaFactoryImageOptionsCover,
+    /**菊花加载*/
+    MediaFactoryImageOptionsIndicator,
+    /**菊花+遮罩加载*/
+    MediaFactoryImageOptionsCoverAndIndicator,
+    /**进度条加载*/
+    MediaFactoryImageOptionsProgressBar,
+    /**遮罩+进度条加载*/
+    MediaFactoryImageOptionsCorverAndProgressBar,
+}MediaFactoryImageOptions;
+
 @interface UIButton (MediaFacotry)
 
 @end
--- MediaFacotry/MediaFacotry/MediaButton/MediaButton.m
+++ MediaFacotry/MediaFacotry/MediaButton/MediaButton.m
@@ -8,22 +8,293 @@
 #import <objc/runtime.h>
 #import "MediaButton.h"
 #import "DataDownloadManager.h"
-#define mediaFacotryActivityIndicatorViewTag (10000)
-#define mediaFacotryCoverViewTag (10001)
+#define mediaFactoryActivityIndicatorViewTag (10000)
+#define mediaFactoryCoverViewTag (10001)
+#define mediaFactoryIndicatorViewWithAndHeight (20)
+
+@interface UIView (MediaFactoryView)
+
+/**
+ 加载菊花
+ */
+@property (nonatomic, strong) UIActivityIndicatorView *indicatorView;
+
+/**
+ 遮罩
+ */
+@property (nonatomic, strong) UIView *coverView;
+
+/**
+ 进度条
+ */
+@property (nonatomic, strong) UIProgressView *progressView;
+
+/**
+ 添加菊花
+ */
+- (void)znk_addIndicatorView;
+
+/**
+ 移除菊花
+ */
+- (void)znk_removeIndicatorView;
+
+/**
+ 添加遮罩
+ */
+- (void)znk_addCoverView;
+
+/**
+ 移除遮罩
+ */
+- (void)znk_removeCoverView;
+
+/**
+ 添加进度条
+ */
+- (void)znk_addProgressViewWithProgress:(float)progress;
+
+/**
+ 移除
+ */
+- (void)znk_removeProgressView;
+@end
+
+@implementation UIView (MediaFactoryView)
+
+
+- (void)setIndicatorView:(UIActivityIndicatorView *)indicatorView{
+    objc_setAssociatedObject(self, @selector(indicatorView), indicatorView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIActivityIndicatorView *)indicatorView{
+    return (UIActivityIndicatorView *)objc_getAssociatedObject(self, @selector(indicatorView));
+}
+
+- (void)setCoverView:(UIView *)coverView{
+    objc_setAssociatedObject(self, @selector(coverView), coverView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIView *)coverView{
+    return (UIView *)objc_getAssociatedObject(self, @selector(coverView));
+}
+
+- (void)setProgressView:(UIProgressView *)progressView{
+    objc_setAssociatedObject(self, @selector(progressView), progressView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIProgressView *)progressView{
+    return (UIProgressView *)objc_getAssociatedObject(self, @selector(progressView));
+}
+
+/**
+ 添加菊花
+ */
+- (void)znk_addIndicatorView{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (!self.indicatorView) {
+            self.indicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+            self.indicatorView.translatesAutoresizingMaskIntoConstraints = NO;
+            self.indicatorView.hidesWhenStopped = YES;
+            [self addSubview:self.indicatorView];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.indicatorView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0]];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.indicatorView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.0]];
+        }
+        [self.indicatorView startAnimating];
+    });
+}
+
+/**
+ 移除菊花
+ */
+- (void)znk_removeIndicatorView{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (self.indicatorView) {
+            [self.indicatorView stopAnimating];
+            [self.indicatorView removeFromSuperview];
+            self.indicatorView = nil;
+        }
+    });
+}
+
+/**
+ 添加遮罩
+ */
+- (void)znk_addCoverView{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (!self.coverView) {
+            self.coverView = [[UIView alloc] init];
+            self.coverView.translatesAutoresizingMaskIntoConstraints = NO;
+            self.coverView.backgroundColor = [UIColor colorWithWhite:0.5 alpha:0.3];
+            [self addSubview:self.coverView];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.coverView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.coverView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0]];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.coverView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0]];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.coverView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0]];
+        }
+    });
+}
+
+/**
+ 移除遮罩
+ */
+- (void)znk_removeCoverView{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (self.coverView) {
+            [self.coverView removeFromSuperview];
+            self.coverView = nil;
+        }
+    });
+}
+
+/**
+ 添加进度条
+ */
+- (void)znk_addProgressViewWithProgress:(float)progress{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (!self.progressView) {
+            self.progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
+            self.progressView.backgroundColor = [UIColor colorWithRed:255 / 255.0 green:96 / 255.0 blue:94 / 255.0 alpha:1.0];
+            [self addSubview:self.progressView];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.progressView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0]];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.progressView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0]];
+            [self addConstraint:[NSLayoutConstraint constraintWithItem:self.progressView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0]];
+        }
+        self.progressView.progress = progress;
+        if (progress >= 1.0) {
+            [self znk_removeProgressView];
+        }
+    });
+}
+
+/**
+ 移除
+ */
+- (void)znk_removeProgressView{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (self.progressView) {
+            [self.progressView removeFromSuperview];
+            self.progressView = nil;
+        }
+    });
+}
+
+/**
+ UIImageView、UIButton设置网络图片基方法
+
+ @param URLString 下载路径
+ @param placeholderImage 占位图
+ @param isBackgroundImage UIButton是否为backgroundImage
+ @param options MediaFactoryImageOptions
+ @param completion 完成block
+ */
+- (void)znk_setImageWithURLString:(NSString * _Nullable)URLString placeholderImage:(UIImage * _Nullable)placeholderImage isBackgroundImage:(BOOL)isBackgroundImage options:(MediaFactoryImageOptions)options completion:(void(^)(BOOL finished, NSError * _Nullable error, UIImage * _Nullable image))completion{
+    if (placeholderImage) {
+        if ([self isKindOfClass:[UIImageView class]]) {
+            UIImageView *imageView = (UIImageView *)self;
+            imageView.image = placeholderImage;
+        } else if ([self isKindOfClass:[UIButton class]]){
+            UIButton *imageButton = (UIButton *)self;
+            if (isBackgroundImage) {
+                [imageButton setBackgroundImage:placeholderImage forState:UIControlStateNormal];
+            } else {
+                [imageButton setImage:placeholderImage forState:UIControlStateNormal];
+            }
+        }
+    }
+    if (!URLString || [URLString isEqualToString:@""] || ![URLString hasPrefix:@"http://"] || ![URLString hasPrefix:@"https://"]) {
+        return;
+    }
+}
+
+#pragma mark - 数据请求，处理
+- (void)znk_imageModelWithURLString:(NSString *)URLString completion:(void(^)(DataDownloadState state, float progress, NSString *filePath, NSError *error))completion{
+    DataDownloadManager *manager = [DataDownloadManager defaultManager];
+    DataDownloadModel *model = [manager currentDownloadingModelWithURLString:URLString];
+    if (model) {
+        if (model.state == DataDownloadStateReady || model.state == DataDownloadStateRunning) {
+            DataDownloadProgress *progress = [manager currentProgressWithDownloadModel:model];
+            if (completion) {
+                completion(model.state, progress.progress, nil, nil);
+            }
+            return;
+        }
+        if (model.state == DataDownloadStateSuspended) {
+            [manager resumeDownloadWithModel:model];
+            if (completion) {
+                __weak typeof(model) weakModel = model;
+                model.stateBlock = ^(DataDownloadState state, NSString *filePath, NSError *error) {
+                    if (state == DataDownloadStateCompleted) {
+                        completion(state, 1.0, filePath, nil);
+                    }else if (state == DataDownloadStateFailed){
+                        completion(state, 0.0, nil, error);
+                    }
+                };
+                model.progressBlock = ^(DataDownloadProgress *progress) {
+                    completion(weakModel.state, progress.progress, nil, nil);
+                };
+            }
+            return;
+        }
+        if ([manager isDownloadCompletedWithDownloadModel:model]) {
+            if (completion) {
+                completion(DataDownloadStateCompleted, 1.0f, model.filePath, nil);
+            }
+            return;
+        }
+        [self startDownloadWithModel:model completion:completion];
+        return;
+    }
+    model = [[DataDownloadModel alloc] initWithURLString:URLString];
+    if ([manager isDownloadCompletedWithDownloadModel:model]) {
+        if (completion) {
+            completion(model.state, 1.0f, model.filePath, nil);
+        }
+        return;
+    }
+    [self startDownloadWithModel:model completion:completion];
+}
+
+- (void)startDownloadWithModel:(DataDownloadModel *)model completion:(void(^)(DataDownloadState state, float progress, NSString *filePath, NSError *error))completion{
+    DataDownloadManager *manager = [DataDownloadManager defaultManager];
+    [manager downloadWithModel:model downloadProgress:^(DataDownloadProgress *progress) {
+        if (completion) {
+            completion(model.state, progress.progress, nil, nil);
+        }
+    } downloadState:^(DataDownloadState state, NSString *filePath, NSError *error) {
+        if (completion) {
+            if (error) {
+                completion(state, 0.0, nil, error);
+            } else {
+                completion(state, 1.0, filePath, nil);
+            }
+        }
+    }];
+}
+
+
+/**
+ 根据URLString获取DataDownloadModel，用户下载，暂停，删除
+
+ @param URLString 下载路径
+ @return DataDownloadModel
+ */
+- (DataDownloadModel *)downloadModelWithURLString:(NSString *)URLString{
+    return [[DataDownloadManager defaultManager] currentDownloadingModelWithURLString:URLString];
+}
+
+@end
 
 
 @implementation UIButton (MediaFacotry)
 
 
-- (DataDownloadModel *)imageModelWithURLString:(NSString *)URLString{
-    DataDownloadModel *model = [[DataDownloadManager defaultManager] currentDownloadingModelWithURLString:URLString];
-    return nil;
-    
-}
-
 - (void)znk_setImageWithURL:(NSString *)URLString forState:(UIControlState)state{
     
 }
+
+
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
