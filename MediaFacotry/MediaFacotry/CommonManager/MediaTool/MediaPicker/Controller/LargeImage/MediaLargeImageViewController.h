5f7503a1b38c74436754e4609776a48b824d1039
znk1007 <1032479745@qq.com>
2017-11-30 21:05:46 +0800

    0.0.1

---
+++ MediaFacotry/MediaFacotry/CommonManager/MediaTool/MediaPicker/Controller/LargeImage/MediaLargeImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MediaLargeImageViewController.h
+//  MediaFacotry
+//
+//  Created by HuangSam on 2017/11/30.
+//  Copyright © 2017年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaLargeImageViewController : UIViewController
+
+@end
