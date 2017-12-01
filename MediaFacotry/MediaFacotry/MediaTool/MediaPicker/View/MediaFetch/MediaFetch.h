12655e6dfbce6b0e676cd9619cfaaa53dbfd29ab
znk1007 <1032479745@qq.com>
2017-12-01 13:39:49 +0800

    0.0.1

---
+++ MediaFacotry/MediaFacotry/MediaTool/MediaPicker/View/MediaFetch/MediaFetch.h
@@ -0,0 +1,13 @@
+//
+//  MediaFetch.h
+//  MediaFacotry
+//
+//  Created by HuangSam on 2017/12/1.
+//  Copyright © 2017年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaFetch : UIView
+
+@end
---
+++ MediaFacotry/MediaFacotry/MediaTool/MediaPicker/View/MediaFetch/MediaFetch.m
@@ -0,0 +1,33 @@
+//
+//  MediaFetch.m
+//  MediaFacotry
+//
+//  Created by HuangSam on 2017/12/1.
+//  Copyright © 2017年 HM. All rights reserved.
+//
+
+#import "MediaFetch.h"
+@interface MediaFetch()
+
+/**
+ 预览视图
+ */
+@property (nonatomic, strong) UICollectionView *previewCollectionView;
+
+/**
+ 相机，相册，取消table
+ */
+@property (nonatomic, strong) UITableView *fetchTable;
+@end
+
+@implementation MediaFetch
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
